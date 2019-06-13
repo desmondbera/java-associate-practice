cc7227f6a5e7cbb174e4961a4da05cec402255b1
Desmond Beramendi <desmond.bera@gmail.com>
2019-06-12 20:57:14 -0400

    MyDate Exercise

--- AcmeOrderSystem/src/MyDate.java
+++ AcmeOrderSystem/src/MyDate.java
@@ -1,4 +1,34 @@
 
 public class MyDate {
-
+	int day;
+	int year;
+	int month;
+	
+	//Initialization block 
+	{
+		day = 1;
+		year = 2000;
+		month = 1;
+	}
+	
+	//Default Constructor
+	public MyDate() {
+	
+	}
+	//Constructor 2
+	public MyDate(int month, int day, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
+	
+	public String toString() {
+		return month + "/" + day + "/" + year;
+	}
+	
+	public void setDate(int month, int day, int year) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+	}
 }
